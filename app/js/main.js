fe74235df0aac86fb658fc5b0ca03191eafa4211
Ganna-S <gannashutenko@gmail.com>
2020-08-13 16:26:17 +0300

    bestsellers added

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,18 @@
 $(function () {
 
-$('.main-screen__slider').slick({
+    $('.main-screen__slider').slick({
         dots: true,
         arrows: false,
         slidesToShow: 1,
         slidesToScroll: 1
+    });
+    $('.bestsellers__slider').slick({
+        dots: true,
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: '<button class="slick-arrow slick-prew"><img src="../images/icons/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="../images/icons/arrow-right.svg" alt=""></button>',
+        
     });
 });
