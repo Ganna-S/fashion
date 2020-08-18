3bf0d49b61646c20a6cb90c4f2d7c2d0229bcb31
Ganna-S <gannashutenko@gmail.com>
2020-08-18 20:15:14 +0300

    start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 $(function () {
 
+    $('.header__menu-btn').on('click', function () {
+        $('.header__box-menu').addClass('active');
+    });
+    $('.close-btn').on('click', function () {
+        $('.header__box-menu').removeClass('active');
+    });
+
     $('.main-screen__slider').slick({
         dots: true,
         arrows: false,
@@ -13,7 +20,45 @@ $(function () {
         slidesToScroll: 4,
         prevArrow: '<button class="slick-arrow slick-prew"><img src="../images/icons/arrow-left.svg" alt=""></button>',
         nextArrow: '<button class="slick-arrow slick-next"><img src="../images/icons/arrow-right.svg" alt=""></button>',
-        
+        responsive: [
+            {
+                breakpoint: 1301,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                }
+            },
+            {
+                breakpoint: 1051,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 711,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                    arrows: false,
+                }
+            },
+        ]
+
     });
     $('.about__slider').slick({
         dots: false,
@@ -22,6 +67,32 @@ $(function () {
         slidesToScroll: 1,
         prevArrow: '<button class="slick-arrow slick-prew"><img src="../images/icons/arrow-left.svg" alt=""></button>',
         nextArrow: '<button class="slick-arrow slick-next"><img src="../images/icons/arrow-right.svg" alt=""></button>',
-        
+        responsive: [
+            {
+                breakpoint: 1301,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 1051,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 785,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                }
+            },
+        ]
+
     });
 });
